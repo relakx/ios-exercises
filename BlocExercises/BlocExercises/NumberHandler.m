1048b7de395583cff27a41f580673e347b2feb65
Joseph Blanco <joefblanco@gmail.com>
2015-04-25 23:54:13 -0400

    Completed simple data types and objects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,34 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    NSNumber *bigNumber = @([number intValue]*2);
+
+    return bigNumber;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    NSMutableArray *numberArray = [NSMutableArray new]; //create array to drop numbers from range into
+    NSInteger lowNum = number;
+    NSInteger highNum = otherNumber;
+    
+    if (number > otherNumber){ //check to see if the 1st number was higher
+        lowNum = otherNumber;
+        highNum = number;
+    }
+    
+    for (NSInteger i = lowNum;i<=highNum;i++){
+        NSNumber *myNum = [NSNumber numberWithInteger:i]; 
+        [numberArray addObject:myNum]; //add the integer
+    }
+    return numberArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    NSMutableArray *lowestNumberArray = [arrayOfNumbers mutableCopy];
+    NSSortDescriptor *sortNumbers = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES];
+    [lowestNumberArray sortUsingDescriptors:@[sortNumbers]];
+    
+    return [lowestNumberArray[0] longValue];
 }
 
 @end
